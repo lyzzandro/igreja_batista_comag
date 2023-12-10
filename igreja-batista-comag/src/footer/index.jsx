0912bfa5d4ed4d298aee774e74b81089a1b39b33
lyzzandro <lyzzandro91@gmail.com>
2023-12-10 09:41:39 -0300

    corrigindo links footer

--- igreja-batista-comag/src/footer/index.jsx
+++ igreja-batista-comag/src/footer/index.jsx
@@ -12,37 +12,73 @@ function SectionFooter() {
         <div className="links">
           <div className="col-links">
             <p className="title-links">Contato</p>
-            <a href="" className="link">
+            <a
+              href="mailto:ibcomag@gmail.com"
+              target="__blank"
+              className="link"
+            >
               <p>ibcomag@gmail.com</p>
             </a>
-            <a href="" className="link">
-              <p>+55 (83) 99109-0097</p>
+            <a
+              href="https://web.whatsapp.com/send?phone=5583991940303"
+              target="__blank"
+              className="link"
+            >
+              <p>+55 (83) 99194-0303</p>
             </a>
-            <a href="" className="link">
+            <a
+              href="https://web.whatsapp.com/send?phone=5583991607575"
+              target="__blank"
+              className="link"
+            >
               <p>+55 (83) 99160-7575</p>
             </a>
           </div>
           <div className="col-links">
             <p className="title-links">Endereço</p>
-            <a href="" className="link">
+            <a
+              href="https://maps.app.goo.gl/b82L8EK2iUGraoDg7"
+              target="__blank"
+              className="link"
+            >
               <p>Rua Adilmar de Paiva Gadelha, 01</p>
             </a>
-            <a href="" className="link">
+            <a
+              href="https://maps.app.goo.gl/b82L8EK2iUGraoDg7"
+              target="__blank"
+              className="link"
+            >
               <p>Bairro André Gadelha</p>
             </a>
-            <a href="" className="link">
+            <a
+              href="https://maps.app.goo.gl/b82L8EK2iUGraoDg7"
+              target="__blank"
+              className="link"
+            >
               <p>Sousa, PB</p>
             </a>
           </div>
           <div className="col-links">
             <p className="title-links">Redes Sociais</p>
-            <a href="" className="link">
+            <a
+              href="https://ibcomag.page.link/instagram"
+              target="__blank"
+              className="link"
+            >
               <p>Instagram</p>
             </a>
-            <a href="" className="link">
+            <a
+              href="https://ibcomag.page.link/youtube"
+              target="__blank"
+              className="link"
+            >
               <p>Youtube</p>
             </a>
-            <a href="" className="link">
+            <a
+              href="https://www.facebook.com/ibcomag"
+              target="__blank"
+              className="link"
+            >
               <p>Facebook</p>
             </a>
           </div>
@@ -66,24 +102,40 @@ function SectionFooter() {
             </a>
 
             <div>
-              <p className="link-site">ibcomag.com.br</p>
+              <p className="link-site">ibcomag.org.br</p>
               <p className="info-site">
-                Copyright © 2022 Igreja Batista Comunidade André Gadelha
+                Copyright © 2024 Igreja Batista Comunidade André Gadelha
               </p>
               <p className="info-site">Todos os direitos reservados</p>
             </div>
           </div>
           <div className="social-media">
-            <a href="#" className="link-logos">
+            <a
+              href="mailto:ibcomag@gmail.com"
+              target="__blank"
+              className="link-logos"
+            >
               <img src={email} alt="Logo email" />
             </a>
-            <a href="#" className="link-logos">
+            <a
+              href="https://ibcomag.page.link/instagram"
+              target="__blank"
+              className="link-logos"
+            >
               <img src={instagram} alt="Logo instagram" />
             </a>
-            <a href="#" className="link-logos">
+            <a
+              href="https://www.facebook.com/ibcomag"
+              target="__blank"
+              className="link-logos"
+            >
               <img src={facebook} alt="Logo facebook" />
             </a>
-            <a href="#" className="link-logos">
+            <a
+              href="https://ibcomag.page.link/youtube"
+              target="__blank"
+              className="link-logos"
+            >
               <img src={youtube} alt="Logo youtube" />
             </a>
           </div>

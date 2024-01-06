16e316a52d5a596c5273145e1a4b2894798c98e5
lyzzandro <lyzzandro91@gmail.com>
2024-01-06 19:30:51 -0300

    estilizando hero

--- igreja-batista-comag/src/section-hero/index.jsx
+++ igreja-batista-comag/src/section-hero/index.jsx
@@ -5,7 +5,7 @@ function SectionHero() {
     <div className="section-hero">
       <div className="container">
         <div className="header-hero">
-          <h1>Igreja Batista COMAG</h1>
+          <h1>IBCOMAG</h1>
           <h2>Uma igreja que ama, uma igreja que ora!</h2>
         </div>
 

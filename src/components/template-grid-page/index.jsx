8ee283843cd00376b3f3067f51eebe187837f5ea
Pedro <pedrojuniorifpb@gmail.com>
2024-02-11 15:49:29 -0300

    Adicionando loading na pagina de template

--- src/components/template-grid-page/index.jsx
+++ src/components/template-grid-page/index.jsx
@@ -1,21 +1,43 @@
+import { useState } from "react";
+
 import AvisoPaginaVazia from "../aviso-pagina-vazia";
+import loadingAnimation from "/loading.gif";
 import "./style.css";
 
+import api from "../../api";
+
 function TemplateGrid({ SectionGrid, listData, title, singleName }) {
+  const [loading, setLoading] = useState(true);
+
+  api.interceptors.response.use((response) => {
+    setLoading(false);
+    return response;
+  }, null);
+
+  function ComponentRender() {
+    return listData.length > 0 ? (
+      listData.map((data, index) => (
+        <SectionGrid key={`grid-${index}`} {...data} />
+      ))
+    ) : (
+      <AvisoPaginaVazia text={`Nenhum ${singleName} encontrado`} />
+    );
+  }
+
   return (
     <div className="main grid">
       <div className="container">
         <div className="content">
           <h4 className="bold title">{title}</h4>
-          <div className="section-grid-item">
-            {listData.length > 0 ? (
-              listData.map((data, index) => (
-                <SectionGrid key={`grid-${index}`} {...data} />
-              ))
-            ) : (
-              <AvisoPaginaVazia text={`Nenhum ${singleName} encontrado`} />
-            )}
-          </div>
+          {loading ? (
+            <div className="item-center">
+              <img src={loadingAnimation} alt="loading..." />
+            </div>
+          ) : (
+            <div className="section-grid-item">
+              <ComponentRender />
+            </div>
+          )}
         </div>
       </div>
     </div>
